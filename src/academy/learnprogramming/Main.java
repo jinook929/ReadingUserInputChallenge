a91aa96061cc1ae571bca8f7bcf1c62325c0e2df
jinook929 <jinook929@gmail.com>
2019-10-14 23:07:46 -0400

    Refactored

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -7,27 +7,26 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner (System.in);
+
         int counter = 0;
         int sum = 0;
-        int num;
 
         while (counter < 10) {
             System.out.print("Enter number #" + (counter + 1) + ": ");
 
             if (scanner.hasNextInt()) {
-                num = scanner.nextInt();
-                scanner.nextLine();
+                int num = scanner.nextInt();
                 sum += num;
                 counter++;
             } else {
-                System.out.println("Invalid Value");
-                scanner.nextLine();
+                System.out.println("Invalid Value. Enter a NUMBER!");
             }
+
+            scanner.nextLine();
         }
 
-        System.out.println("Sum of Ten Numbers: " + sum);
+        System.out.println("Sum of the Ten Numbers: " + sum);
 
         scanner.close();
     }
-
 }
